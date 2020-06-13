75f5241a1d87b98279abffd2155966853575d5b2
EstebanKalil <estekgamer@gmail.com>
2020-06-13 15:46:43 -0500

    low-level algothims and doing hello world project

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - printsa  text with printf()
+ * Return: 0
+ */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+my firts project using c
